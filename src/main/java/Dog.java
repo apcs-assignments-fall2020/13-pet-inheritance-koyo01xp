c5b52278f13b9b52ceacc3e398e55de98c0908b2
koyo01xp <21kasakawa@athenian.org>
2020-10-29 19:32:42 +0000

    homework

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,35 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String a, int b, String c) {
+        super(a,b);
+        this.breed = c;
+    }
 
+    public Dog() {
+        super("Bob",3);
+        this.breed = "poodle";
+    }    
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Woof-Woof");
+    }
 
     // toString method
-
+    public String toString() {
+        String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + this.breed;
+        return str;
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        this.breed = breed; 
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,9 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Pet a = new Pet();
+        System.out.println(a);
+
+        Pet b = new Dog("Bob",3,"poodle");
+        System.out.println(b);
     }
 }
